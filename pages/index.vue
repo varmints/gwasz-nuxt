6496cc29b5ff1cd4c490b116e1b60c48678fe3bc
Kamil <example@example.com>
2025-03-10 00:24:52 +0100

    change fonts provider

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
 useSeoMeta({
-  title: 'Pracownia Gwasz. Kursy malarstwa i rysunku',
-  ogTitle: 'Pracownia Gwasz. Kursy malarstwa i rysunku',
-  description: 'Kursy malarstwa i rysunku Dla dzieci, młodzieży i dorosłych. Zapisz się na zajęcia próbne.',
-  ogDescription: 'Kursy malarstwa i rysunku Dla dzieci, młodzieży i dorosłych. Zapisz się na zajęcia próbne.',
+  title: 'Pracownia Gwasz - kursy malarstwa i rysunku',
+  ogTitle: 'Pracownia Gwasz - kursy malarstwa i rysunku',
+  description: 'Kursy malarstwa i rysunku dla dzieci, młodzieży i dorosłych. Zapisz się na zajęcia próbne.',
+  ogDescription: 'Kursy malarstwa i rysunku dla dzieci, młodzieży i dorosłych. Zapisz się na zajęcia próbne.',
   ogImage: '',
 })
 </script>

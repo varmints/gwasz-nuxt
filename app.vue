fc09bf019ba61219dbd3cab2c2a4622a14b329aa
Kamil <example@example.com>
2025-03-10 21:53:31 +0100

    add favicon

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+useHead({
+  meta: [
+    { name: 'apple-mobile-web-app-title', content: 'Gwasz' },
+  ],
+})
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLayout>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/fonts', '@unocss/nuxt', 'v-gsap-nuxt', '@nuxt/icon'],
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
+    },
+  },
   css: ['@unocss/reset/tailwind-compat.css', '~/assets/css/main.css'],
   fonts: {
     families: [{ name: 'Inter Tight', provider: 'fontsource' }, { name: 'Marker Felt', provider: 'local' }],
